dea30de69dd5efa60e56234aac5b1fdc871c75ea
shuarz <shuarz.wolfz@gmail.com>
2022-04-06 19:46:43 -0300

    Modulos

---
+++ Curso/Ejercicios/FuncionesMatematicas.py
@@ -0,0 +1,18 @@
+def suma(num1, num2): 
+
+	print("el resultado es: ", num1+num2)
+
+
+def resta(num1, num2): 
+
+	print("el resultado es: ", num1-num2)
+
+
+def multiplicacion(num1, num2): 
+
+	print("el resultado es: ", num1*num2)
+
+
+def division(num1, num2): 
+
+	print("el resultado es: ", num1/num2)
---
+++ Curso/Ejercicios/Modulos.py
@@ -0,0 +1,11 @@
+#import FuncionesMatematicas #otra opcion posible
+
+from FuncionesMatematicas import * #con el asterisco llamamos a todas las funciones que tenga el otro modulo, en este caso, funionesmatematicas
+
+# primero usamos el from, modulo que utilizaremos, despues import y el asterisco si queremos todo o solo la clase o funcion que necesitemos
+
+suma(7 ,5)
+
+resta(9, 5)
+
+multiplicacion(5 ,6)
