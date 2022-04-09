fb9ce33f55b57ad28a89bfd8fef2b7dd9d049898
shuarz <shuarz.wolfz@gmail.com>
2022-04-09 12:54:28 -0300

    Plus: potenciar + redondeo

--- Curso/Ejercicios/FuncionesMatematicas.py
+++ Curso/Ejercicios/FuncionesMatematicas.py
@@ -16,3 +16,9 @@ def multiplicacion(num1, num2):
 def division(num1, num2): 
 
 	print("el resultado es: ", num1/num2)
+
+def potencia(base, exponente):
+	print("el resultado de la potencia es: ", base**exponente)
+
+def redondear(numero):
+	print("el resultado del redondeo es: ", round(numero))
