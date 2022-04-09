77b87db0e5775f68a2866efe00ffc8419aa9127c
shuarz <shuarz.wolfz@gmail.com>
2022-04-09 13:01:42 -0300

    Paquetes

---
+++ Curso/Ejercicios/Calculadora/CalculosGenerales.py
@@ -0,0 +1,24 @@
+def suma(num1, num2): 
+
+	print("el resultado es: ", num1+num2)
+
+
+def resta(num1, num2): 
+
+	print("el resultado es: ", num1-num2)
+
+
+def multiplicacion(num1, num2): 
+
+	print("el resultado es: ", num1*num2)
+
+
+def division(num1, num2): 
+
+	print("el resultado es: ", num1/num2)
+
+def potencia(base, exponente):
+	print("el resultado de la potencia es: ", base**exponente)
+
+def redondear(numero):
+	print("el resultado del redondeo es: ", round(numero))
---
+++ Curso/Ejercicios/UsoModulo.py
@@ -0,0 +1,3 @@
+from Calculadora.CalculosGenerales import potencia
+
+# un paquete se crea con crear un archivo dentro con un __init__.py
