9e3d3522b1b2fb935dc6113fdbb75f12a78c9dff
shuarz <shuarz.wolfz@gmail.com>
2022-04-09 14:19:02 -0300

    LLamando paquete

--- Curso/Ejercicios/UsoModulo.py
+++ Curso/Ejercicios/UsoModulo.py
@@ -1,3 +1,6 @@
-from Calculadora.CalculosGenerales import potencia
+from Calculadora.CalculosGenerales import division #o ponemos el asterisco y usamos todas las funciones
+# ponemos el nombre de la carpeta y despues el punto
+division(3, 9)
+
 
 # un paquete se crea con crear un archivo dentro con un __init__.py
